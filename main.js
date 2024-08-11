49ab97620003edfcf38440ce17488e887bfc1e0a
matveygal <mgalikt@gmail.com>
2024-08-11 23:50:53 +0200

    tweaks for school

--- main.js
+++ main.js
@@ -8,11 +8,16 @@ export const ctx = canvas.getContext('2d')
 
 
 let nodes = [
-    new Node(400, 350), new Node(750, 100),
-    new Node(1100, 350)]
+    new Node(100, 350), new Node(350, 100),
+    new Node(600, 350)]
 nodes.forEach(node => {drawCircle(node.x, node.y, 6, '#000000', ctx)});
 
 let resi = new Resistor(nodes[0], nodes[1])
 let didi = new Diode(nodes[1], nodes[2])
 resi.draw(100, ctx)
 didi.draw(100, ctx)
+
+let rectifierNodes = [new Node(700, 350), new Node(850, 200), new Node(1000, 350), new Node(850, 500)]
+rectifierNodes.forEach(node => {drawCircle(node.x, node.y, 6, '#000000', ctx)});
+let rectifierDiodes = [new Diode(rectifierNodes[0], rectifierNodes[1]), new Diode(rectifierNodes[1], rectifierNodes[2]), new Diode(rectifierNodes[0], rectifierNodes[3]), new Diode(rectifierNodes[3], rectifierNodes[2])]
+rectifierDiodes.forEach(diode => {diode.draw(60, ctx)})
